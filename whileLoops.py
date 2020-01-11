89abb4940d449c8cf37c420917bf997f20dc0d18
Emilio Araos <ea3dev@gmail.com>
2020-01-11 13:35:04 -0700

    pass, continue, break

---
+++ whileLoops.py
@@ -0,0 +1,21 @@
+x = 0
+
+while x < 5:
+	print('THE CURRENT VALUE OF X IS {}'.format(x))
+	x = x + 1    # This is the same as Javva,   x += 1
+else:
+	print('x is not less than 5')
+
+x = [1,2,4]
+
+for item in x:
+	pass
+
+print('end of my script')
+
+myString = 'Sammy'
+
+for letter in myString:
+	if letter =='a':
+		continue
+	print(letter)
